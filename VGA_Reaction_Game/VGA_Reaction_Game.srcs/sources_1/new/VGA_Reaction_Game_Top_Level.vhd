ed43091177e2ea0061c55add918207fc57b50b34
adalik108 <adalik108@users.noreply.github.com>
2018-11-14 17:43:18 -0700

    added VGA_Reaction_Game_Top_Level file

---
+++ VGA_Reaction_Game/VGA_Reaction_Game.srcs/sources_1/new/VGA_Reaction_Game_Top_Level.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/14/2018 05:42:10 PM
+-- Design Name: 
+-- Module Name: VGA_Reaction_Game_Top_Level - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity VGA_Reaction_Game_Top_Level is
+--  Port ( );
+end VGA_Reaction_Game_Top_Level;
+
+architecture Behavioral of VGA_Reaction_Game_Top_Level is
+
+begin
+
+
+end Behavioral;
