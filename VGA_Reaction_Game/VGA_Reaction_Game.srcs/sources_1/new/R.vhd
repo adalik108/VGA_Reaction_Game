68192a017956f085eebd2ca1037db650a870d913
adalik108 <adalik108@users.noreply.github.com>
2018-11-15 23:32:25 -0700

    Update R.vhd

--- VGA_Reaction_Game/VGA_Reaction_Game.srcs/sources_1/new/R.vhd
+++ VGA_Reaction_Game/VGA_Reaction_Game.srcs/sources_1/new/R.vhd
@@ -68,6 +68,24 @@ component Rectangle is
       );
 end component;
 
+component Diagonal2 is
+    Generic ( 	d2_height: integer:= 10;
+        d2_width: integer:= 10;
+        d2_x_offset: integer:= 50;
+        d2_y_offset: integer:= 50);
+    Port (     clk : in  STD_LOGIC;
+        reset : in  STD_LOGIC;
+        scan_line_x: in STD_LOGIC_VECTOR(10 downto 0);
+        scan_line_y: in STD_LOGIC_VECTOR(10 downto 0);
+        rectangle_color: in STD_LOGIC_VECTOR(11 downto 0);
+        --rectangle_height: in STD_LOGIC_VECTOR(8 downto 0);
+        kHz: in STD_LOGIC;
+        red: out STD_LOGIC_VECTOR(3 downto 0);
+        blue: out STD_LOGIC_VECTOR(3 downto 0);
+        green: out std_logic_vector(3 downto 0)
+);
+end component;
+
 signal i_red1, i_green1, i_blue1: std_logic_vector(3 downto 0);
 signal i_red2, i_green2, i_blue2: std_logic_vector(3 downto 0);
 signal i_red3, i_green3, i_blue3: std_logic_vector(3 downto 0);
@@ -76,7 +94,7 @@ signal i_red5, i_green5, i_blue5: std_logic_vector(3 downto 0);
 signal i_red6, i_green6, i_blue6: std_logic_vector(3 downto 0);
 
 constant tail_height: integer:= 2;
-constant tail_width: integer:= 2;
+constant tail_width: integer:= 1;
 constant long: integer:= 10;
 constant half_length: integer:= 5;
 constant narrow: integer:= 2;
@@ -165,12 +183,12 @@ Middle: Rectangle
                   green           => i_green4
         );
         
-Tail_1: Rectangle
+Tail_1: Diagonal2
       Generic Map(     
-                  rectangle_height    => tail_height,
-                  rectangle_width     => tail_width,
-                  x_offset            => px_offset + half_length + tail_width,
-                  y_offset            => py_offset + long + tail_height + long
+                  d2_height    => tail_height,
+                  d2_width     => tail_width,
+                  d2_x_offset            => px_offset + half_length,
+                  d2_y_offset            => py_offset + long - narrow + tail_height
                   )
       Port Map(     
                   clk             => clk,
